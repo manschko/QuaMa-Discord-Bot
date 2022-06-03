aa06c75906e91e6f657649b390d855b0ae38868c
manschko <manschko1999@googlemail.com>
2022-06-03 14:23:46 +0200

    Added Slash command for master Channel

---
+++ schema.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const voice_channel_id = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    }
+})
+
+const config = new mongoose.Schema({
+    temp_master_channel:{
+        type: [{
+            id: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            }
+        }],
+        required: false
+    }
+})
+
+export default {
+    voice_channel_id: mongoose.model('voice_channel_id', voice_channel_id, 'voice_channel_ids'),
+    config: mongoose.model('config', config, 'configs'),
+    }
